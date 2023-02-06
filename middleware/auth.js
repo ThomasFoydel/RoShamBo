80f3791af857529c09fee7e155245e3cd24318ce
ThomasFoydel <thomasjfoydel@gmail.com>
2023-02-06 00:06:22 -0500

    refactored auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,17 @@
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken')
 
 module.exports = function (req, res, next) {
-  const token = req.header('x-auth-token');
-  if (!token) {
-    return res.status(401).json({ msg: 'Authorization denied' });
-  }
+  const token = req.header('x-auth-token')
+  if (!token) return res.status(401).json({ msg: 'Authorization denied' })
+
   try {
-    const decoded = jwt.verify(token, process.env.SECRET);
-    req.tokenUser = decoded.tokenUser;
-    next();
+    const decoded = jwt.verify(token, process.env.SECRET)
+    req.tokenUser = decoded.tokenUser
+    const currentTime = Date.now() / 1000
+    if (decoded.exp >= currentTime) throw new Error('TokenExpiredError')
+    next()
   } catch (err) {
-    if (err.name === 'TokenExpiredError') {
-      return res.status(401).send({ msg: 'token expired' });
-    }
-    res.status(401).json({ msg: 'Authorizaton denied' });
+    if (err.message === 'TokenExpiredError') return res.status(401).send({ msg: 'token expired' })
+    return res.status(401).json({ msg: 'Authorizaton denied' })
   }
-};
+}
