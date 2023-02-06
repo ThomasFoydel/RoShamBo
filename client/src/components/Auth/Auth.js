555ac0527c5356cdcbaad750ee333c1f628b3a0b
ThomasFoydel <thomasjfoydel@gmail.com>
2023-02-06 02:51:43 -0500

    refactored auth

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,31 +1,29 @@
-import React, { useState, useContext } from 'react';
-import axios from 'axios';
-import { Redirect } from 'react-router-dom';
-import { Modal, makeStyles, Snackbar } from '@material-ui/core';
-import Alert from '@material-ui/lab/Alert';
-import { CTX } from 'context/Store';
-import Register from './Register';
-import Login from './Login';
+import axios from 'axios'
+import Alert from '@material-ui/lab/Alert'
+import { Redirect } from 'react-router-dom'
+import React, { useState, useContext } from 'react'
+import { Modal, makeStyles, Snackbar } from '@material-ui/core'
+import { CTX } from 'context/Store'
+import Register from './Register'
+import Login from './Login'
 
 const useStyles = makeStyles((theme) => ({
   modalBody: {
-    position: 'absolute',
+    transform: `translate(-50%, -50%)`,
     backgroundColor: theme.palette.background.paper,
     border: `2px solid ${theme.palette.common.blue}`,
     boxShadow: theme.shadows[5],
-    top: '50%',
+    position: 'absolute',
     left: '50%',
-    transform: `translate(-50%, -50%)`,
+    top: '50%',
   },
-  modal: {
-    zIndex: '9000 !important',
-  },
-}));
+  modal: { zIndex: '9000 !important' },
+}))
 
 const initialState = {
   register: {
-    email: '',
     name: '',
+    email: '',
     password: '',
     confirmpassword: '',
   },
@@ -33,80 +31,58 @@ const initialState = {
     email: '',
     password: '',
   },
-};
+}
 
 const Auth = () => {
-  const [appState, updateState] = useContext(CTX);
-  const classes = useStyles();
-  let { authModal } = appState;
-  const [authPage, setAuthPage] = useState('login');
-  const [formState, setFormState] = useState(initialState);
-  const [err, setErr] = useState(null);
-  const [redirect, setRedirect] = useState(false);
+  const [{ authModal }, updateState] = useContext(CTX)
+  const [formState, setFormState] = useState(initialState)
+  const [authPage, setAuthPage] = useState('login')
+  const [redirect, setRedirect] = useState(false)
+  const [err, setErr] = useState(null)
+  const classes = useStyles()
 
-  const closeModal = () => updateState({ type: 'AUTH_MODAL', payload: false });
+  const closeModal = () => updateState({ type: 'AUTH_MODAL', payload: false })
 
   const handleAuth = (type) => {
     axios
       .post(`/api/auth/${type}`, formState[type])
       .then(({ data }) => {
-        const { token, user } = data;
-        if (type === 'register') setRedirect(true);
-        updateState({ type: 'LOGIN', payload: { token, user } });
-        setFormState(initialState);
+        const { token, user } = data
+        if (type === 'register') setRedirect(true)
+        updateState({ type: 'LOGIN', payload: { token, user } })
+        setFormState(initialState)
       })
-      .catch(
-        ({
-          response: {
-            data: { err },
-          },
-        }) => err && setErr(err)
-      );
-  };
+      .catch((err) => setErr(err.data.err))
+  }
+
+  const props = { formState, handleAuth, setAuthPage, setFormState }
+
   const ModalBody = (
     <div className={classes.modalBody}>
-      {authPage === 'register' ? (
-        <Register
-          props={{
-            handleAuth,
-            setAuthPage,
-            formState,
-            setFormState,
-          }}
-        />
-      ) : (
-        <Login
-          props={{
-            handleAuth,
-            setAuthPage,
-            formState,
-            setFormState,
-          }}
-        />
-      )}
+      {authPage === 'register' ? <Register props={props} /> : <Login props={props} />}
     </div>
-  );
+  )
 
-  const closeErr = () => setErr(null);
+  const closeErr = () => setErr(null)
 
   return (
     <Modal className={classes.modal} open={authModal} onClose={closeModal}>
       <>
-        {redirect && <Redirect to='/howto' />}
+        {redirect && <Redirect to="/howto" />}
         {ModalBody}
         <Snackbar
           open={err}
-          autoHideDuration={5000}
           onClose={closeErr}
+          autoHideDuration={5000}
           anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
         >
-          <Alert onClose={closeErr} severity='error'>
+          <Alert onClose={closeErr} severity="error">
             {err}
           </Alert>
         </Snackbar>
       </>
     </Modal>
-  );
-};
+  )
+}
 
-export default Auth;
+export default Auth
