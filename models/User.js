f1ba5a44e0c93cd7c1ef2ee852e87fb6b78d6679
ThomasFoydel <thomasjfoydel@gmail.com>
2023-02-06 01:24:27 -0500

    refactored user model

--- models/User.js
+++ models/User.js
@@ -1,26 +1,27 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs')
+const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: [true, 'username required'],
     validate: {
       validator: (str) => {
-        return str.length >= 5 && str.length <= 10;
+        return str.length >= 5 && str.length <= 10
       },
       message: 'username must be between 5 and 10 characters',
     },
   },
   email: {
     type: String,
-    required: true,
+    required: [true, 'email required'],
     unique: true,
     select: false,
     validate: {
       validator: (str) => {
-        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(String(str).toLowerCase());
+        const re =
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        return re.test(String(str).toLowerCase())
       },
       message: 'must be a valid email',
     },
@@ -29,8 +30,9 @@ const userSchema = mongoose.Schema({
     type: String,
     validate: {
       validator: (str) => {
-        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(String(str).toLowerCase());
+        const re =
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        return re.test(String(str).toLowerCase())
       },
       message: 'must be a valid email',
     },
@@ -47,7 +49,7 @@ const userSchema = mongoose.Schema({
   },
   password: {
     type: String,
-    required: [true, 'Password required'],
+    required: [true, 'password required'],
     select: false,
   },
   friends: {
@@ -58,21 +60,21 @@ const userSchema = mongoose.Schema({
     type: Number,
     default: 0,
   },
-});
+})
 
 userSchema.pre('save', async function save(next) {
-  if (!this.isModified('password')) return next();
+  if (!this.isModified('password')) return next()
   try {
-    const salt = await bcrypt.genSalt(12);
-    this.password = await bcrypt.hash(this.password, salt);
-    return next();
+    const salt = await bcrypt.genSalt(12)
+    this.password = await bcrypt.hash(this.password, salt)
+    return next()
   } catch (err) {
-    return next(err);
+    return next(err)
   }
-});
+})
 
 userSchema.methods.validatePassword = async function validatePassword(data) {
-  return bcrypt.compare(data, this.password);
-};
+  return bcrypt.compare(data, this.password)
+}
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema)
