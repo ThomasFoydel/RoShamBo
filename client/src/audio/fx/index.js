63050f11a1a127f757c9cb65add34cc9d61823a5
ThomasFoydel <thomasjfoydel@gmail.com>
2023-02-08 03:02:13 -0500

    refactored audio index files

--- client/src/audio/fx/index.js
+++ client/src/audio/fx/index.js
@@ -1,21 +1,21 @@
-import count from './count.mp3';
-import shoot from './shoot.mp3';
-import shootLong from './shootLong.mp3';
-import fight from './fight.mp3';
-import fightShort from './fightShort.mp3';
-import win from './win.mp3';
-import lose from './lose.mp3';
-import tie from './tie.mp3';
+import tie from './tie.mp3'
+import win from './win.mp3'
+import lose from './lose.mp3'
+import count from './count.mp3'
+import shoot from './shoot.mp3'
+import fight from './fight.mp3'
+import shootLong from './shootLong.mp3'
+import fightShort from './fightShort.mp3'
 
 const fx = {
-  count: new Audio(count),
-  shoot: new Audio(shoot),
-  shootLong: new Audio(shootLong),
-  fight: new Audio(fight),
-  fightShort: new Audio(fightShort),
+  tie: new Audio(tie),
   win: new Audio(win),
   lose: new Audio(lose),
-  tie: new Audio(tie),
-};
+  count: new Audio(count),
+  shoot: new Audio(shoot),
+  fight: new Audio(fight),
+  shootLong: new Audio(shootLong),
+  fightShort: new Audio(fightShort),
+}
 
-export default fx;
+export default fx
--- client/src/audio/weapons/index.js
+++ client/src/audio/weapons/index.js
@@ -1,15 +1,15 @@
-import rock from './rock.mp3';
-import paper from './paper.mp3';
-import scissors from './scissors.mp3';
-import tree from './tree.mp3';
-import bird from './bird.mp3';
+import rock from './rock.mp3'
+import tree from './tree.mp3'
+import bird from './bird.mp3'
+import paper from './paper.mp3'
+import scissors from './scissors.mp3'
 
 const weapons = {
   rock: new Audio(rock),
-  paper: new Audio(paper),
-  scissors: new Audio(scissors),
   tree: new Audio(tree),
   bird: new Audio(bird),
-};
+  paper: new Audio(paper),
+  scissors: new Audio(scissors),
+}
 
-export default weapons;
+export default weapons
