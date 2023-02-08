0cdc724aff90ed7f172bd7d45a7d7175055ffe84
ThomasFoydel <thomasjfoydel@gmail.com>
2023-02-08 03:34:57 -0500

    refactored message notification

--- client/src/components/MessageNotification/MessageNotification.js
+++ client/src/components/MessageNotification/MessageNotification.js
@@ -1,18 +1,14 @@
-import React, { useContext, useState } from 'react';
-import { Snackbar } from '@mui/material';
-import { Alert } from '@mui/lab';
-import { Link } from 'react-router-dom';
-import { CTX } from 'context/Store';
-const MessageNotification = ({
-  props: {
-    message: { sender, content, senderId },
-    close,
-  },
-}) => {
-  const [, updateState] = useContext(CTX);
+import { Alert } from '@mui/lab'
+import { Link } from 'react-router-dom'
+import { Snackbar } from '@mui/material'
+import React, { useContext } from 'react'
+import { CTX } from 'context/Store'
 
-  const handleClick = () =>
-    updateState({ type: 'CURRENT_THREAD', payload: senderId });
+const MessageNotification = ({ props: { message, close } }) => {
+  const { sender, content, senderId } = message
+  const [, updateState] = useContext(CTX)
+
+  const handleClick = () => updateState({ type: 'CURRENT_THREAD', payload: senderId })
 
   return (
     <Snackbar
@@ -22,19 +18,14 @@ const MessageNotification = ({
       anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
     >
       {content && (
-        <Alert
-          onClose={close}
-          severity='info'
-          style={{ border: '1px solid black' }}
-        >
-          <Link to='/messages' onClick={handleClick}>
-            {sender}:{' '}
-            {content.length < 20 ? content : `${content.substring(0, 20)}...`}
+        <Alert onClose={close} severity="info" style={{ border: '1px solid black' }}>
+          <Link to="/messages" onClick={handleClick}>
+            {sender}: {content.length < 20 ? content : `${content.substring(0, 20)}...`}
           </Link>
         </Alert>
       )}
     </Snackbar>
-  );
-};
+  )
+}
 
-export default MessageNotification;
+export default MessageNotification
