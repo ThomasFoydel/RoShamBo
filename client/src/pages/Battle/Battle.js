364210cc43b72695e4367667f795e558ff0ce657
ThomasFoydel <thomasjfoydel@gmail.com>
2023-02-10 14:50:59 -0500

    fixed battle page styles

--- client/src/pages/Battle/Battle.js
+++ client/src/pages/Battle/Battle.js
@@ -7,37 +7,39 @@ const styles = (theme) => ({
   battlePage: {
     ...theme.centerHorizontal,
     background: 'linear-gradient(#ccc, #ddd)',
-  },
-  battleLink: {
-    margin: '1em',
+    width: '90%',
+    padding: '2.5rem',
+    marginTop: '5rem',
+    borderRadius: '4px',
   },
   link: {
     padding: '1em',
     borderRadius: '4px',
     transition: 'all 0.3s ease',
+    boxShadow: 'inset 4px 4px 30px #00000025, 5px 5px 30px #00000000',
     '&:hover': {
-      background: 'linear-gradient(to bottom right, #111, #222)',
-      '& $text': {
-        color: theme.palette.primary.light,
+      boxShadow: 'inset -5px -5px 30px #00000015, 5px 5px 30px #00000040',
+      p: {
+        color: theme.palette.primary.main,
       },
-      '& $icon': {
+      svg: {
         color: theme.palette.primary.main,
       },
     },
   },
   text: {
-    transition: 'all 0.3s ease',
-    color: theme.palette.primary.dark,
     fontSize: '2rem',
     fontWeight: 'bold',
+    transition: 'all 0.3s ease',
+    color: theme.palette.primary.dark,
     [theme.breakpoints.down('xs')]: {
       fontSize: '1.4rem',
     },
   },
   icon: {
-    transition: 'all 0.3s ease',
     width: '100%',
     height: '100%',
+    transition: 'all 0.3s ease',
     color: theme.palette.primary.dark,
   },
 })
@@ -47,29 +49,33 @@ const options = [
   { link: 'computer', text: 'Computer', icon: Computer },
   { link: 'random', text: 'Random User', icon: Casino },
 ]
+
 const Battle = () => {
   const classes = useClasses(styles)
-  
+
   return (
-    <Grid container direction="row" justify="space-around" className={classes.battlePage}>
-      {options.map((option) => {
-        const Icon = option.icon
-        return (
-          <Grid key={option.link} item className={classes.battleLink}>
-            <Grid
-              container
-              direction="column"
-              alignItems="center"
-              component={Link}
-              to={`/battle/${option.link}`}
-              className={classes.link}
-            >
-              <Icon className={classes.icon} />
-              <Typography className={classes.text}>{option.text}</Typography>
-            </Grid>
+    <Grid
+      container
+      gap="1.5em"
+      direction="row"
+      justifyContent="center"
+      className={classes.battlePage}
+    >
+      {options.map(({ link, icon: Icon, text }) => (
+        <Grid key={link} item>
+          <Grid
+            container
+            component={Link}
+            direction="column"
+            alignItems="center"
+            to={`/battle/${link}`}
+            className={classes.link}
+          >
+            <Icon className={classes.icon} />
+            <Typography className={classes.text}>{text}</Typography>
           </Grid>
-        )
-      })}
+        </Grid>
+      ))}
     </Grid>
   )
 }
