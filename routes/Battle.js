7d76525ef3e70ea7cfb6088d3914ef22f52806fa
ThomasFoydel <thomasjfoydel@gmail.com>
2023-02-12 16:55:41 -0500

    converted battle routes to async await

--- routes/Battle.js
+++ routes/Battle.js
@@ -5,25 +5,29 @@ const auth = require('../middleware/auth')
 const router = express.Router()
 
 router.get('/:friendshipId', auth, async (req, res) => {
-  const { friendshipId } = req.params
-  const { userId } = req.tokenUser
-  API.friendship
-    .findById(friendshipId)
-    .then(({ participants, _id, sender, receiver }) => {
-      if (!participants.includes(userId)) return res.sendStatus(401)
-      return res.status(200).send({
-        status: 'success',
-        message: 'Friendship found',
-        success: true,
-        roomId: _id,
-        users: [sender, receiver],
-      })
+  try {
+    const { friendshipId } = req.params
+    const { userId } = req.tokenUser
+    const friendship = await API.friendship.findById(friendshipId)
+    if (!friendship) {
+      return res.status(404).send({ status: 'error', message: 'Friendship not found' })
+    }
+    const { participants, _id, sender, receiver } = friendship
+    if (!participants.includes(userId)) {
+      return res.status(401).send({ status: 'error', message: 'Not authorized' })
+    }
+    return res.status(200).send({
+      status: 'success',
+      message: 'Friendship found',
+      success: true,
+      roomId: _id,
+      users: [sender, receiver],
     })
-    .catch(() =>
-      res
-        .status(500)
-        .send({ status: 'error', message: 'Database is down, we are working to fix this' })
-    )
+  } catch (err) {
+    return res
+      .status(500)
+      .send({ status: 'error', message: 'Database is down, we are working to fix this' })
+  }
 })
 
 module.exports = router
