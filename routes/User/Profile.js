059713a1311cd73df1d386078a98036842b2339d
ThomasFoydel <thomasjfoydel@gmail.com>
2023-02-12 16:42:12 -0500

    converted profile routes to async await

--- routes/User/Profile.js
+++ routes/User/Profile.js
@@ -6,35 +6,43 @@ const auth = require('../../middleware/auth')
 const router = express.Router()
 
 router.get('/:profileId', async (req, res) => {
-  const userId = req.header('userId')
-  const { profileId } = req.params
-  let profileObjId
-  let userObjId
   try {
-    profileObjId = new mongoose.Types.ObjectId(profileId)
-    if (userId !== 'null' && userId !== 'undefined') userObjId = new mongoose.Types.ObjectId(userId)
+    const userId = req.header('userId')
+    const { profileId } = req.params
+    let profileObjId
+    let userObjId
+    try {
+      profileObjId = new mongoose.Types.ObjectId(profileId)
+      if (userId !== 'null' && userId !== 'undefined')
+        userObjId = new mongoose.Types.ObjectId(userId)
+    } catch (err) {
+      return res.status(404).send({ status: 'error', message: 'Invalid ID(s)' })
+    }
+    const existingFriendship = await API.friendship.findByUsers(userObjId, profileObjId)
+    const friendshipExists = !!existingFriendship
+    const userProfile = await API.user.findById(profileObjId)
+    return res
+      .status(201)
+      .send({ status: 'success', message: 'Profile found', user: userProfile, friendshipExists })
   } catch (err) {
-    return res.status(404).send({ status: 'error', message: 'Invalid ID(s)' })
+    return res
+      .status(500)
+      .send({ status: 'error', message: 'Database is down, we are working to fix this' })
   }
-  const existingFriendship = await API.friendship.findByUsers(userObjId, profileObjId)
-  const friendshipExists = !!existingFriendship
-  const userProfile = await API.user.findById(profileObjId)
-  res
-    .status(201)
-    .send({ status: 'success', message: 'Profile found', user: userProfile, friendshipExists })
 })
 
-router.put('/', auth, (req, res) => {
-  const { userId } = req.tokenUser
-  const update = req.body
-  API.user
-    .updateProfile(userId, update)
-    .then((user) => res.status(200).send({ status: 'success', message: 'Profile updated', user }))
-    .catch(() =>
-      res
-        .status(500)
-        .send({ status: 'error', message: 'Database is down, we are working to fix this' })
-    )
+router.put('/', auth, async (req, res) => {
+  try {
+    const { userId } = req.tokenUser
+    const update = req.body
+    const user = await API.user.updateProfile(userId, update)
+    if (!user) return res.status(404).send({ status: 'error', message: 'User not found' })
+    return res.status(200).send({ status: 'success', message: 'Profile updated', user })
+  } catch (err) {
+    return res
+      .status(500)
+      .send({ status: 'error', message: 'Database is down, we are working to fix this' })
+  }
 })
 
 module.exports = router
